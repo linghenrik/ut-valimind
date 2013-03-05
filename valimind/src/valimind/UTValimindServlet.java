9a73d90f0749e2ea75a42c7c654011ae12024901
allarsoo <allar.soo@gmail.com>
2013-03-05 21:34:00 +0200

    Added our WEB project files, they are mainly in war folder, but also in the src folder, there lies our server. NB! When deploying into Google Appengine please change the version value!

---
+++ valimind/src/valimind/UTValimindServlet.java
@@ -0,0 +1,13 @@
+package valimind;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class UTValimindServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}

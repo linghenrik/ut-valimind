a48be485400980a51642198088c339aab1b3191a
allarsoo <allar.soo@gmail.com>
2013-03-27 23:42:30 +0200

    Möllasin natuke servletidega, hetkel rohkem ei jõua, neljapäeval tegutsen edasi, üritan htmli ja JS ka need suhtlema panna

---
+++ valimind/src/utvalimind/servlet/CreateNewIsikServlet.java
@@ -0,0 +1,66 @@
+package utvalimind.servlet;
+import com.google.appengine.api.rdbms.AppEngineDriver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*This servlet tries to create a new person, its called out when someone
+ *	is logged in, then the someone needs to enter his/her personal information 
+ * to be able to vote(Id, Name, Surname, DOB)
+ */
+	
+	
+public class CreateNewIsikServlet {
+	
+	private long Id;
+	private String nimi;
+	private String perenimi;
+	DateFormat df = new SimpleDateFormat("yyyy/dd/MM");
+	private Date DOB;
+	public void doPost (HttpServletRequest req, HttpServletResponse resp)
+			  throws IOException, ParseException{
+		  Connection connection = null;
+		  PrintWriter out=resp.getWriter();
+		    try {
+		      DriverManager.registerDriver(new AppEngineDriver());
+		      connection = DriverManager.getConnection("jdbc:google:rdbms://valmindbyut:valimindbyut/evalimised");
+		      Id=Long.parseLong(req.getParameter("ID")); 
+		      nimi=req.getParameter("nimi");
+		      perenimi=req.getParameter("perenimi"); //Insert those two into Isik
+		      DOB=(Date) df.parse(req.getParameter("dob"));
+		      
+				if(nimi=="" || perenimi=="" || DOB==null ){
+					out.println("<html><head></head><body>You are missing prameters!Try again!!...</body></html>");
+				}
+				else{
+					Statement stmt = null;
+					PreparedStatement ps = null;
+					String sql;
+					sql="insert into Isik(Id,Eesnimi,Perenimi,DOB) values("+Id+","+nimi+","+perenimi+","+DOB+")";
+					ps= connection.prepareStatement(sql);
+					stmt=connection.createStatement();
+				}
+	}
+		    catch(SQLException e){
+		    	out.println("<html><head></head><body>Ilmenes viga, see on nüüd küll piinlik.</body></html>");
+		    }
+		    finally{
+				if(connection != null){
+					try{
+						connection.close();
+					}
+					catch(SQLException ignore){
+						
+					}
+				}
+			}
+}
+	
+}
---
+++ valimind/src/utvalimind/servlet/FindByRegion.java
@@ -0,0 +1,63 @@
+package utvalimind.servlet;
+import com.google.appengine.api.rdbms.AppEngineDriver;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FindByRegion {
+	private String regionName;
+	//private List<T> list=new ArrayList<T>();
+	Gson gson=new Gson();
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException{
+			regionName=req.getParameter("regioon");
+			boolean proceed=false;
+			if(regionName != null){
+				if(regionName.length()>0){
+					proceed=true;
+				}
+			}
+			Connection con = null;
+			Statement stmt = null;
+			ResultSet rs = null;
+			PreparedStatement ps = null;
+			try{
+				DriverManager.registerDriver(new AppEngineDriver());
+				con = DriverManager.getConnection("jdbc:google:rdbms://valmindbyut:valimindbyut/evalimised");
+				stmt=con.createStatement();
+				rs=stmt.executeQuery("select Kandidaat.Id, Eesnimi, Perenimi from Kandidaat, Isik where Kandidaat.Regioon=(select Id from Regioon where Nimi='"+regionName+ "') and Kandidaat.Isik=Isik.Id");
+				Collection collection= new ArrayList();
+				collection.add(rs.getObject(1));
+				collection.add(rs.getObject(2));
+				collection.add(rs.getObject(3));
+				collection.add(regionName);
+				gson.toJson(collection);
+				res.setContentType("application/json");
+			    res.setCharacterEncoding("UTF-8");
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			finally{
+				if(con != null){
+					try{
+						con.close();
+					}
+					catch(SQLException ignore){
+						
+					}
+				}
+			}
+			
+			}
+	}

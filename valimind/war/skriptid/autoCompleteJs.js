01e3c0bc5845a9c7c10676aed5c64ea91ecabc54
kyng <kyng386@hotmail.com>
2013-04-29 09:32:58 +0300

    autocomplete vajab on() funktsiooni.

--- valimind/war/skriptid/autoCompleteJs.js
+++ valimind/war/skriptid/autoCompleteJs.js
@@ -1,7 +1,14 @@
-$(function() {
+$('tags').on(function(){
+	$(this).autocomplete({
+		source: function(request, response) {
+			$.getJSON("/AutocompleteServlet", { lastName: request.term }, response);
+	}});
+});
+/*$(function() {
 		$( "#tags" ).autocomplete({
 		source: function(request, response) {
 		    $.getJSON("/AutocompleteServlet", { lastName: request.term }, response);
 		  }
 	});
 });
+*/
